64e41e1c19a00d65a36b5a6079e226e9fdefd3e6
Glitch (juicy-simplistic-wannanosaurus) <none>
2022-04-19 14:31:10 +0000

    😻🏁 Checkpoint ./js/index.js:31431774/1

--- js/index.js
+++ js/index.js
@@ -223,7 +223,7 @@ function loadMovieDetails() {
       searchList.classList.add("hide-search-list");
       movieSearchBox.value = "";
       const result = await fetch(
-        `http://www.omdbapi.com/?i=${movie.dataset.id}&apikey=1a0304e4`
+        `https://www.omdbapi.com/?i=${movie.dataset.id}&apikey=1a0304e4`
       );
       const movieDetails = await result.json();
       details = movieDetails;
